a48c7b56eeb922a8be433578f213e9beb1e6c016
Alphano FABIEN <alphano-externe.fabien@enedis.fr>
2020-04-27 17:48:54 +0200

    Refacto in functional style

--- src/main/java/com/github/alphaon/euler/problems/P035.java
+++ src/main/java/com/github/alphaon/euler/problems/P035.java
@@ -3,11 +3,11 @@ package com.github.alphaon.euler.problems;
 
 import com.github.alphaon.euler.lib.Library;
 
-import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static com.github.alphaon.euler.lib.Tuples.t2;
+import static com.github.alphaon.euler.lib.Tuples.t3;
 
 /**
  * see: <a href="https://projecteuler.net/problem=35">Circular primes</a>
@@ -16,72 +16,34 @@ public class P035 {
 
     private final Library lib = Library.newInstance();
 
-    private int[] generateNumbers(int size) {
-        int[] ret = new int[(int) Math.pow(5, size)];
-        for (int i = 0; i < size; i++) {
-            ret[0] = 10 * ret[0] + 1;
-        }
-        for (int i = 1; i < ret.length; i++) {
-            int n = ret[i - 1];
-            int q = 1;
-            int r = 0;
-            for (int j = 0; j < size && n % 10 == 9; j++) {
-                r = 10 * r + 1;
-                n /= 10;
-                q *= 10;
-            }
-            ret[i] = (n + 2) * q + r;
-        }
-        return ret;
+    private IntStream generateNumbers(int size) {
+        int M = (int) Math.pow(10, size);
+        return IntStream.iterate((M - 1) / 9, nn -> nn < M, nn ->
+                Stream.iterate(t3(nn, 1, 0), t -> t3(t.a / 10, t.b * 10, 10 * t.c + 1))
+                        .dropWhile(t -> t.a % 10 == 9)
+                        .findFirst().or(() -> Optional.of(t3(nn, 1, 0)))
+                        .map(t -> (t.a + 2) * t.b + t.c)
+                        .orElseThrow());
     }
 
-    private int[] permutations(int N) {
-        int size = 1;
-        int q = 1;
-        for (int nn = N; nn >= 10; nn /= 10) {
-            size++;
-            q *= 10;
-        }
-        int[] ret = new int[size];
-        ret[0] = N;
-        boolean foundCycle = false;
-        for (int i = 1; i < size && !foundCycle; i++) {
-            int v = 10 * (ret[i - 1] % q) + ret[i - 1] / q;
-            foundCycle = v == N;
-            if (!foundCycle) {
-                ret[i] = v;
-            }
-        }
-        return ret;
+    private IntStream permutations(int N) {
+        int q = IntStream.iterate(N, nn -> nn >= 10, nn -> nn / 10).map(__ -> 10).reduce(1, (a, b) -> a * b);
+        return IntStream.concat(IntStream.of(N),
+                IntStream.iterate(10 * (N % q) + N / q, nn -> nn != N, nn -> 10 * (nn % q) + nn / q)
+        );
     }
 
     private boolean isCircularPrime(int N) {
-        int[] permutations = permutations(N);
-        boolean ret = true;
-        for (int i = 0; i < permutations.length && permutations[i] != 0 && ret; i++) {
-            ret = lib.isPrimeNumber(permutations[i]);
-        }
-        return ret;
+        return permutations(N).allMatch(lib::isPrimeNumber);
     }
 
     private int nbCircularPrimesWithSize(int size) {
-        int[] numbers = generateNumbers(size);
-        int count = 0;
-        for (int N : numbers) {
-            if (isCircularPrime(N)) {
-                count++;
-            }
-        }
-        return count;
+        return (int) generateNumbers(size).filter(this::isCircularPrime).count();
     }
 
     private int nbCircularPrimesWithMaxSize(int maxSize) {
         if (maxSize <= 0) return 0;
-        int count = 4; //2, 3, 5, 7
-        for (int size = 2; size <= maxSize; size++) {
-            count += nbCircularPrimesWithSize(size);
-        }
-        return count;
+        return IntStream.rangeClosed(2, maxSize).map(this::nbCircularPrimesWithSize).sum() + 4;
     }
 
 
